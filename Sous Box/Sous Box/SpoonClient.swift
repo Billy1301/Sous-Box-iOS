4f378f84bc3a55f168cc220f0b1cb6371aba1269
Billy <mr.bill.ly@gmail.com>
2017-03-27 16:33:14 -0700

    Spoon client
    worked on spoon api constants

--- Sous Box/Sous Box/SpoonClient.swift
+++ Sous Box/Sous Box/SpoonClient.swift
@@ -30,5 +30,46 @@ class Spoonacular {
         return _title
     }
     
+    var id: Int {
+        if _id == nil {
+            _id = 0
+        }
+        return _id
+    }
+    
+    var image: String {
+        if _image == nil {
+            _image = ""
+        }
+        return _image
+    }
+    
+    var imageUrls: String {
+        if _imageUrls == nil {
+            _imageUrls = ""
+        }
+        return _imageUrls
+    }
+    
+    var originalString: String {
+        if _originalString == nil {
+            _originalString = ""
+        }
+        return _originalString
+    }
+    
+    var ingredientImage: String {
+        if _ingredientImage == nil {
+            _ingredientImage = ""
+        }
+        return _ingredientImage
+    }
+    
+    var name: String {
+        if _name == nil {
+            _name = ""
+        }
+        return _name
+    }
     
 }
